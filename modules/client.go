e41526e840a45779f037698a1c8d78cf5acd4d01
Amir Iravanimanesh <airavanimanesh@gmail.com>
2021-12-05 10:07:01 +0330

    feat: launch a simple echo server

--- modules/client.go
+++ modules/client.go
@@ -5,11 +5,12 @@ import (
 	"net"
 )
 
-func Abbas() {
-	hostName := "example.com"
-	portNum := "6000"
+func RunClient(port string) {
+	var msg string
 
-	conn, err := net.Dial("tcp", hostName+":"+portNum)
+	hostName := "localhost"
+
+	conn, err := net.Dial("tcp", hostName+":"+port)
 
 	if err != nil {
 		fmt.Println(err)
@@ -19,5 +20,25 @@ func Abbas() {
 	fmt.Printf("Connection established between %s and localhost.\n", hostName)
 	fmt.Printf("Remote Address : %s \n", conn.RemoteAddr().String())
 	fmt.Printf("Local Address : %s \n", conn.LocalAddr().String())
+
+	for {
+		fmt.Print("Enter message: ")
+		fmt.Scanln(&msg)
+		_, err := conn.Write([]byte(msg))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		buf := make([]byte, 4096)
+		_, err = conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Print("Message received:")
+			fmt.Println(string(buf))
+		}
+	}
 
 }
--- modules/server.go
+++ modules/server.go
@@ -27,20 +27,33 @@ func RunServer(port string) {
 func handleConnection(c net.Conn) {
 
 	log.Printf("Client %v connected.", c.RemoteAddr())
-
-	buffer := make([]byte, 4096)
+	sendMessage(c, "Welcome to the golculator!\nFirst send me a number and then an operator.\nThen send me another number.\n")
 
 	for {
-		n, err := c.Read(buffer)
-		if err != nil || n == 0 {
-			c.Close()
-			break
-		}
-		_, err = c.Write(buffer[0:n])
-		if err != nil {
-			c.Close()
-			break
-		}
+		clientMessage := readMessage(c)
+		log.Printf("Client %v sent: %s", c.RemoteAddr(), clientMessage)
+
+		sendMessage(c, clientMessage)
+		log.Printf("Server response: %s", clientMessage)
 	}
 	log.Printf("Connection from %v closed.", c.RemoteAddr())
+}
+
+func sendMessage(c net.Conn, message string) {
+	_, err := c.Write([]byte(message))
+	if err != nil {
+		log.Printf("Error writing to client: %s", err)
+		c.Close()
+	}
+}
+
+func readMessage(c net.Conn) string {
+	buffer := make([]byte, 4096)
+	n, err := c.Read(buffer)
+	if err != nil || n == 0 {
+		log.Printf("Error reading from client: %s", err)
+		c.Close()
+		return ""
+	}
+	return string(buffer[:n])
 }
