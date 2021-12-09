205f5aa7d21079bcc4e44db4c092b6e00f6ca536
Amir Iravanimanesh <airavanimanesh@gmail.com>
2021-12-09 20:38:14 +0330

    feat: making calculator components

---
+++ internal/server/numerical/handler.go
@@ -0,0 +1,25 @@
+package numerical
+
+import "strconv"
+
+func Calculate(num1, num2 float64, op string) float64 {
+	switch op {
+	case "+":
+		return Add(num1, num2)
+	case "-":
+		return Sub(num1, num2)
+	case "*":
+		return Mul(num1, num2)
+	case "/":
+		return Div(num1, num2)
+	}
+	return 0
+}
+
+func ParseNumber(message string) (float64, error) {
+	num, err := strconv.ParseFloat(message, 64)
+	if err != nil {
+		return 0, err
+	}
+	return num, nil
+}
---
+++ internal/server/numerical/ops.go
@@ -0,0 +1,17 @@
+package numerical
+
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Sub(a, b float64) float64 {
+	return a - b
+}
+
+func Mul(a, b float64) float64 {
+	return a * b
+}
+
+func Div(a, b float64) float64 {
+	return a / b
+}
